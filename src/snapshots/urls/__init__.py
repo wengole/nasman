d3b2b531b7efc4de4c6ae4f778a13b8e71a93ab2
Ben Cole <wengole@gmail.com>
2015-02-19 22:23:56 +0000

    Added - namespace to snapshot urls

--- src/snapshots/urls/__init__.py
+++ src/snapshots/urls/__init__.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include
 
-urlpatterns = patterns('',
-
-    (r'^snapshots/', include('snapshots.urls.snapshot_urls')),  # NOQA
-    (r'^files/', include('snapshots.urls.file_urls')),
+urlpatterns = patterns(
+    '',
+    (r'^snapshots/', include('snapshots.urls.snapshot_urls', namespace='snapshots')),
+    (r'^files/', include('snapshots.urls.file_urls', namespace='files')),
 )
