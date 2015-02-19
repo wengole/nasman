7e9c58c7b7ab91790d1aed3767e45d74c95985e2
Ben Cole <wengole@gmail.com>
2015-02-19 16:48:05 +0000

    Added - Models generate from "Models in no time" site

--- src/wizfs/snapshots/models.py
+++ src/wizfs/snapshots/models.py
@@ -5,4 +5,35 @@ class Snapshot(models.Model):
     """
     Model representing s ZFS snapshot
     """
+    name = models.CharField('name', max_length=255)
+    timestamp = models.DateTimeField('timestamp')
 
+    class Meta:
+        unique_together = ()
+        index_together = ()
+
+    def __unicode__(self):
+        return self.name
+
+
+class File(models.Model):
+    full_path = models.CharField('full path', max_length=255)
+    snapshot = models.ForeignKey(
+        Snapshot,
+        verbose_name='snapshot',
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE
+    )
+    mime_type = models.CharField('mime type', blank=True, max_length=255)
+    extension = models.CharField('extension', blank=True, max_length=255)
+    created = models.DateTimeField('created')
+    modified = models.DateTimeField('modified')
+    size = models.IntegerField("size", blank=True, null=True)
+
+    class Meta:
+        unique_together = ()
+        index_together = ()
+
+    def __unicode__(self):
+        return self.full_path
