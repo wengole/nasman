42da02896cdefc42d27c50a4ffa5ffb012ba38cb
Ben Cole <wengole@gmail.com>
2015-02-19 22:24:41 +0000

    Added - menu for snapshots

---
+++ src/snapshots/menus.py
@@ -0,0 +1,35 @@
+from django.core.urlresolvers import reverse
+
+from menu import MenuItem
+from menu import Menu
+
+
+def snapshots_menu(request):
+    menu = []
+    menu.append(
+        MenuItem(
+            title='Snapshots',
+            url='#',
+            classes='dropdown-header'
+        )
+    )
+    menu.append(
+        MenuItem(
+            title='List Snapshots',
+            url=reverse('snapshots:snapshot_list'),
+        )
+    )
+    return menu
+
+
+Menu.add_item(
+    'top_nav_left',
+    MenuItem(
+        title='Snapshots',
+        url='#',
+        children=snapshots_menu,
+        classes='dropdown',
+        #TODO: Restrict showing menu
+        # check=lambda x: True
+    )
+)
