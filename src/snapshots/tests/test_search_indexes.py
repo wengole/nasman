e83091215a26a9136a31d842d8029380d7fec1e4
Ben Cole <wengole@gmail.com>
2015-02-22 10:18:39 +0000

    Fixed - search indexes test

---
+++ src/snapshots/tests/test_search_indexes.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+from haystack.utils.loading import UnifiedIndex
+
+from django.test import override_settings, TestCase
+
+import haystack
+from haystack.query import SearchQuerySet
+
+from ..models import Snapshot
+from snapshots.search_indexes import SnapshotIndex
+
+
+TEST_INDEX = {
+    'default': {
+        'ENGINE': 'xapian_backend.XapianEngine',
+        'PATH': 'test_index',
+    },
+}
+
+
+@override_settings(HAYSTACK_CONNECTIONS=TEST_INDEX)
+class SearchQuerySetTestCase(TestCase):
+    # fixtures = ['bulk_data.json']
+
+    def setUp(self):
+        super(SearchQuerySetTestCase, self).setUp()
+
+        # Stow.
+        self.old_unified_index = haystack.connections['default']._index
+        self.ui = UnifiedIndex()
+        self.ui.build()
+        haystack.connections['default']._index = self.ui
+
+        # Update the "index".
+        backend = haystack.connections['default'].get_backend()
+        backend.clear()
+        backend.update(SnapshotIndex, Snapshot.objects.all())
+
+        self.msqs = SearchQuerySet()
+
+        # Stow.
+        haystack.reset_search_queries()
+
+    def tearDown(self):
+        # Restore.
+        haystack.connections['default']._index = self.old_unified_index
+        super(SearchQuerySetTestCase, self).tearDown()
+
+    def test_snapshot_is_indexed(self):
+        sqs = SearchQuerySet().all()
+        self.assertEqual(sqs.count(), 0)
+        Snapshot.objects.create(
+            name='test_snap_1',
+            timestamp=datetime.now()
+        )
+        sqs = SearchQuerySet().all()
+        self.assertEqual(sqs.count(), 1)
