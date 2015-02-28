0dc94e9889e80e4c9686095c7664c8c33d2a8b3f
Ben Cole <wengole@gmail.com>
2015-02-28 20:20:43 +0000

    Added - file index

--- src/snapshots/search_indexes.py
+++ src/snapshots/search_indexes.py
@@ -1,6 +1,6 @@
 from haystack import indexes
 
-from .models import Snapshot
+from .models import Snapshot, File
 
 
 class SnapshotIndex(indexes.SearchIndex, indexes.Indexable):
@@ -8,8 +8,25 @@ class SnapshotIndex(indexes.SearchIndex, indexes.Indexable):
     Indexer for ``Snapshot`` objects
     """
     text = indexes.CharField(document=True, use_template=True)
-    name = indexes.CharField(model_attr='name')
-    timestamp = indexes.DateTimeField(model_attr='timestamp', null=True)
+    name = indexes.CharField(model_attr=u'name')
+    timestamp = indexes.DateTimeField(model_attr=u'timestamp', null=True)
 
     def get_model(self):
         return Snapshot
+
+
+class FileIndex(indexes.SearchIndex, indexes.Indexable):
+    """
+    Indexer for ``File`` objects
+    """
+    text = indexes.CharField(document=True, use_template=True)
+    directory = indexes.CharField(model_attr=u'dirname', faceted=True)
+    name = indexes.CharField(model_attr=u'name')
+    snapshot = indexes.CharField(
+        model_attr=u'snapshot__name',
+        null=True,
+        faceted=True
+    )
+
+    def get_model(self):
+        return File
