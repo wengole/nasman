622d7086dfe05ecd92e8c3d17856e57b42949eb7
Ben Cole <wengole@gmail.com>
2015-03-01 21:54:39 +0000

    Added - pyinotify to allow celery to autoreload

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         'Celery[redis]',
         'eventlet',
         'flower',
+        'pyinotify',
     ],
     dependency_links=[
         'https://github.com/akatrevorjay/pyzfscore/tarball/master#egg=pyzfscore-0.0.1',
