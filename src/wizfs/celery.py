29c930e10bdfd1fc88b9dba899a1eaab6f682fdd
Ben Cole <wengole@gmail.com>
2015-03-02 08:10:18 +0000

    Fixed - celery to work with configurations

--- src/wizfs/celery.py
+++ src/wizfs/celery.py
@@ -3,16 +3,16 @@ from __future__ import absolute_import
 import os
 
 from celery import Celery
-
 from django.conf import settings
 
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wizfs.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wizfs.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Local')
+
+from configurations import importer
+
+importer.install(check_options=True)
 
 app = Celery('wizfs')
-
-# Using a string here means the worker will not have to
-# pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
