c2bafaa950d99e3989a60fbdbb820084e4bc433a
Ben Cole <wengole@gmail.com>
2015-03-16 09:02:23 +0000

    Overriding ModelAdmin instead of using SearchAdmin and custom search view
    Added - facet info to changelist view
    Removed - checkout of 2.4 of haystack as it's got bugs

--- src/snapshots/admin.py
+++ src/snapshots/admin.py
@@ -1,7 +1,8 @@
 import logging
-from haystack.admin import SearchModelAdmin
+from haystack.admin import SearchModelAdmin, SearchChangeList
 from django.conf import settings
 from django.contrib import admin
+from haystack.query import SearchQuerySet
 
 from .models import Snapshot, File, Filesystem
 from .tasks import reindex_filesystem
@@ -17,10 +18,25 @@ class SnapshotAdmin(admin.ModelAdmin):
 
 
 @admin.register(File)
-class FileAdmin(SearchModelAdmin):
-    list_display = ('name', 'dirname', 'mime_type', 'modified', 'size',)
+class FileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'dirname', 'mime_type',
+                    'modified', 'size',)
+    list_display_links = ('name',)
     search_fields = ['full_path', ]
 
+    def get_search_results(self, request, queryset, search_term):
+        sqs = SearchQuerySet().auto_query(request.GET['q']).facet('directory')
+        return sqs, False
+
+    def get_changelist(self, request, **kwargs):
+        return SearchChangeList
+
+    def changelist_view(self, request, extra_context=None):
+        sqs, _ = self.get_search_results(request, None, 'snapshot')
+        extra_context = {
+            'facets': sqs.query.get_facet_counts()
+        }
+        return super(FileAdmin, self).changelist_view(request, extra_context)
 
 @admin.register(Filesystem)
 class FilesystemAdmin(admin.ModelAdmin):
