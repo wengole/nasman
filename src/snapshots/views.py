5d2643d5ea748a32e2181e810c25d7980b63f304
Ben Cole <wengole@gmail.com>
2015-03-16 09:02:46 +0000

    Removed - custom search view for now

--- src/snapshots/views.py
+++ src/snapshots/views.py
@@ -1,34 +1,3 @@
-from haystack.generic_views import FacetedSearchView
-from haystack.query import EmptySearchQuerySet
-
-
-class SnapshotSearch(FacetedSearchView):
-    """
-    Search view for the Snapshot app
-    """
-    results = EmptySearchQuerySet()
-    queryset = results
-
-    def get_form_kwargs(self):
-        kwargs = super(SnapshotSearch, self).get_form_kwargs()
-        kwargs.update({'data': self.request.REQUEST})
-        return kwargs
-
-    def get_context_data(self, **kwargs):
-        self.results = self.get_queryset()
-        context = super(SnapshotSearch, self).get_context_data(**kwargs)
-        paginator = context['paginator']
-        page = context['page_obj']
-        entries_from = (
-            (
-                (paginator.per_page * (page.number - 1)) + 1)
-            if paginator.count > 0 else 0
-        )
-        entries_to = entries_from - 1 + paginator.per_page
-        if paginator.count < entries_to:
-            entries_to = paginator.count
-        context.update({
-            'title': 'Search',
-            'item_numbers': '%s - %s' % (entries_from, entries_to),
-        })
-        return context
+"""
+Snapshot app views
+"""
--- src/wizfs/urls.py
+++ src/wizfs/urls.py
@@ -2,14 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import RedirectView
-from snapshots.views import SnapshotSearch
 
 
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', RedirectView.as_view(url=reverse_lazy('admin:index'))),
-    # url(r'^snapshots/', include('snapshots.urls', namespace='snapshots')),
-    # url(r'^$', include('base.urls')),
-    url(r'^search/?$', SnapshotSearch.as_view(), name='search'),
 )
