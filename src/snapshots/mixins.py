8051214edf17d4ea206bba7d09779244132ca403
Ben Cole <wengole@gmail.com>
2015-03-30 19:17:16 +0100

    Fixed - if view was a form view, this would break

--- src/snapshots/mixins.py
+++ src/snapshots/mixins.py
@@ -6,11 +6,6 @@ class SearchFormMixin(object):
     Mixin to add search form to any view
     """
     def get_context_data(self, **kwargs):
-        form_class = getattr(self, 'form_class', None)
-        if form_class is None:
-            form_class = CrispyFacetedSearchForm
-        form = form_class()
-
+        form = CrispyFacetedSearchForm()
         return super(SearchFormMixin, self).get_context_data(
             search_form=form, **kwargs)
-
