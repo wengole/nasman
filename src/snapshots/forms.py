c7baa44ff0527e37cd008af4d7b0d56b4a952db5
Ben Cole <wengole@gmail.com>
2015-03-31 21:46:18 +0100

    Unicodify

--- src/snapshots/forms.py
+++ src/snapshots/forms.py
@@ -11,11 +11,11 @@ class FilesystemForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(FilesystemForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
-        self.helper.layout.append(Submit('save', 'save'))
+        self.helper.layout.append(Submit(u'save', u'save'))
 
     class Meta:
         model = Filesystem
-        fields = ['name', 'mountpoint', 'parent']
+        fields = [u'name', u'mountpoint', u'parent']
 
 
 class CrispyFacetedSearchForm(FacetedSearchForm):
@@ -23,17 +23,17 @@ class CrispyFacetedSearchForm(FacetedSearchForm):
         super(CrispyFacetedSearchForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
         self.helper.form_method = u'GET'
-        self.helper.form_action = reverse('wizfs:search')
+        self.helper.form_action = reverse(u'wizfs:search')
         self.helper.layout.append(Submit(u'search', u'search'))
         self.helper.form_class = u'navbar-form navbar-right'
-        self.helper.field_template = 'bootstrap3/layout/inline_field.html'
+        self.helper.field_template = u'bootstrap3/layout/inline_field.html'
 
 
 class SnapshotForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(SnapshotForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
-        self.helper.layout.append(Submit('save', 'save'))
+        self.helper.layout.append(Submit(u'save', u'save'))
 
     class Meta:
         model = Snapshot
