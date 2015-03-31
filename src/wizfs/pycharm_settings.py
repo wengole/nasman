9c7936eef980292d4287ea5a0f22f805f81c3cc9
Ben Cole <wengole@gmail.com>
2015-03-31 13:24:35 +0100

    Need to use an actual cache, but maybe a different redis DB

--- src/wizfs/pycharm_settings.py
+++ src/wizfs/pycharm_settings.py
@@ -117,8 +117,12 @@ SUIT_CONFIG = {
 }
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/2',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
     }
 }
-SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-SESSION_CACHE_ALIAS = "default"
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_CACHE_ALIAS = 'default'
--- src/wizfs/settings.py
+++ src/wizfs/settings.py
@@ -161,7 +161,11 @@ class Test(Common):
     HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
     CACHES = {
         'default': {
-            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+            'BACKEND': 'django_redis.cache.RedisCache',
+            'LOCATION': 'redis://127.0.0.1:6379/2',
+            'OPTIONS': {
+                'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            }
         }
     }
 
