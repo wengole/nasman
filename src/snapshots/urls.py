de196faf08e3537b015d43d66155a28f4f34f17b
Ben Cole <wengole@gmail.com>
2015-04-01 20:19:50 +0100

    Refactored views into multiple modules

--- src/snapshots/urls.py
+++ src/snapshots/urls.py
@@ -1,20 +1,25 @@
 from django.conf.urls import patterns, url
 
-from . import views
+from .views.base import DashboardView
+from .views.filesystems import FileBrowser, FilesystemList, FilesystemDetail
+from .views.filesystems import FilesystemCreate, FilesystemDelete
+from .views.filesystems import FilesystemUpdate
+from .views.search import SnapshotSearchView
+
 
 urlpatterns = patterns(
     '',
-    url(r'^$', views.DashboardView.as_view(), name='dashboard'),
-    url(r'^file-browser$', views.FileBrowser.as_view(), name='file-browser'),
-    url(r'^filesystems$', views.FilesystemList.as_view(), name='filesystems'),
-    url(r'^filesystem/(?P<pk>\d+)$', views.FilesystemDetail.as_view(),
+    url(r'^$', DashboardView.as_view(), name='dashboard'),
+    url(r'^file-browser$', FileBrowser.as_view(), name='file-browser'),
+    url(r'^filesystems$', FilesystemList.as_view(), name='filesystems'),
+    url(r'^filesystem/(?P<pk>\d+)$', FilesystemDetail.as_view(),
         name='filesystem'),
-    url(r'^filesystem/add$', views.FilesystemCreate.as_view(),
+    url(r'^filesystem/add$', FilesystemCreate.as_view(),
         name='add-fs'),
-    url(r'^filesystem/(?P<pk>\d+)/delete$', views.FilesystemDelete.as_view(),
+    url(r'^filesystem/(?P<pk>\d+)/delete$', FilesystemDelete.as_view(),
         name='delete-fs'),
-    url(r'^filesystem/(?P<pk>\d+)/edit$', views.FilesystemUpdate.as_view(),
+    url(r'^filesystem/(?P<pk>\d+)/edit$', FilesystemUpdate.as_view(),
         name='edit-fs'),
-    url(r'^search$', views.SnapshotSearchView.as_view(),
+    url(r'^search$', SnapshotSearchView.as_view(),
         name='search'),
 )
--- src/snapshots/views/filesystems.py
+++ src/snapshots/views/filesystems.py
@@ -1,45 +1,19 @@
-"""
-Snapshot app views
-"""
 from collections import defaultdict
 from datetime import datetime
 import os
 
 from braces.views import JSONResponseMixin, AjaxResponseMixin, MessageMixin
-from celery import Celery
-from celery import states
-from django.conf import settings
 from django.core.urlresolvers import reverse_lazy
 from django.http import Http404
-from django.shortcuts import redirect, get_object_or_404
-from haystack.generic_views import FacetedSearchView
-from haystack.query import SearchQuerySet
+from django.shortcuts import get_object_or_404, redirect
 import magic
 from vanilla import TemplateView, ListView, DetailView, CreateView, DeleteView
 from vanilla import UpdateView
 
-from .views.base import BaseView
-from .utils import ZFSHelper
-from .forms import FilesystemForm, CrispyFacetedSearchForm
-from .models import Filesystem, Snapshot, IconMapping
-
-app = Celery('wizfs')
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-
-def get_status_dict(fs):
-    status = fs.reindex_status
-    json_dict = {'state': 'NOTFOUND', 'progress': 0.0, 'total': 0, 'done': 0}
-    if status is None:
-        return json_dict
-    json_dict['state'] = status.state
-    if status.state in [states.FAILURE, states.PENDING]:
-        return json_dict
-    json_dict['progress'] = status.info.get('percentage', 0)
-    json_dict['total'] = status.info.get('total', 0)
-    json_dict['done'] = status.info.get('done', 0)
-    return json_dict
+from ..forms import FilesystemForm
+from ..models import Filesystem, Snapshot, IconMapping
+from ..utils import ZFSHelper
+from ..views.base import BaseView
 
 
 class FileBrowser(BaseView, TemplateView):
@@ -112,14 +86,6 @@ class FilesystemList(JSONResponseMixin, AjaxResponseMixin, BaseView, ListView):
     model = Filesystem
     headline = u'ZFS Filesystems'
 
-    def get_ajax(self, request, *args, **kwargs):
-        filesystems = []
-        for fs in self.get_queryset().all():
-            json_dict = get_status_dict(fs)
-            json_dict['id'] = fs.id
-            filesystems.append(json_dict)
-        return self.render_json_response({'filesystems': filesystems})
-
 
 class FilesystemDetail(JSONResponseMixin, AjaxResponseMixin, MessageMixin,
                        BaseView, DetailView):
@@ -127,29 +93,6 @@ class FilesystemDetail(JSONResponseMixin, AjaxResponseMixin, MessageMixin,
 
     def get_headline(self):
         return u'%s Filesystem Details' % self.get_object().name
-
-    def get(self, request, *args, **kwargs):
-        """
-        If "reindex" query parameter passed, then reindex the chosen filesystem
-        """
-        if request.GET.get(u'reindex'):
-            fs = self.get_object()
-            status = fs.reindex_status
-            if (status is None
-                    or status.state in states.READY_STATES
-                    or status.state is None):
-                fs.reindex_status = reindex_filesystem.delay(fs.name)
-                self.messages.info(u'Reindex of %s started' % fs.name)
-                return redirect(u'wizfs:filesystems')
-            self.messages.warning(
-                u'Reindex of %s already in progress' % fs.name)
-            return redirect(u'wizfs:filesystems')
-        return super(FilesystemDetail, self).get(request, *args, **kwargs)
-
-    def get_ajax(self, request, *args, **kwargs):
-        fs = self.get_object()
-        json_dict = get_status_dict(fs)
-        return self.render_json_response(json_dict)
 
 
 class FilesystemCreate(MessageMixin, BaseView, CreateView):
@@ -186,28 +129,3 @@ class FilesystemUpdate(BaseView, UpdateView):
 
     def get_headline(self):
         return u'Edit %s filesystem' % self.get_object().name
-
-
-class SnapshotSearchView(BaseView, FacetedSearchView):
-    template = u'search.html'
-    form_class = CrispyFacetedSearchForm
-    results = None
-
-    def get_headline(self):
-        if self.search_field in self.request.REQUEST:
-            return u'Search results'
-        return u'Search'
-
-    def get_form_kwargs(self):
-        sqs = SearchQuerySet().facet(u'directory')
-        kwargs = super(SnapshotSearchView, self).get_form_kwargs()
-        kwargs.update({u'searchqueryset': sqs, u'data': self.request.REQUEST})
-        return kwargs
-
-    def form_valid(self, form):
-        self.results = form.search()
-        return super(SnapshotSearchView, self).form_valid(form)
-
-    def form_invalid(self, form):
-        self.results = form.search()
-        return super(SnapshotSearchView, self).form_invalid(form)
