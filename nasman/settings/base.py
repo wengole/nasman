e0a3f6fb77f7a413d38530c4eb860c2c1dd4794a
Ben Cole <wengole@gmail.com>
2015-04-21 21:36:55 +0100

    We don't have an apps package

--- nasman/settings/base.py
+++ nasman/settings/base.py
@@ -7,8 +7,6 @@ here = lambda *x: join(abspath(dirname(__file__)), *x)
 PROJECT_ROOT = here("..")
 root = lambda *x: join(abspath(PROJECT_ROOT), *x)
 
-sys.path.insert(0, root('apps'))
-
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'CHANGE THIS!!!'
