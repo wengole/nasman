d41e89fc10f1a3207692578b028c1866b0146e28
Ben Cole <wengole@gmail.com>
2015-04-23 14:19:37 +0100

    Added form for file browser

--- nasman/snapshots/forms.py
+++ nasman/snapshots/forms.py
@@ -4,9 +4,17 @@ from .utils.zfs import ZFSUtil
 
 def filesystem_choices():
     filesystems = [x.name for x in ZFSUtil.get_filesystems()]
-    choices = ((None, '-'),)
+    choices = ((None, 'Select filesystem'),)
     return choices + tuple(zip(filesystems, filesystems))
 
+
+class SmallSelectWidget(forms.Select):
+    def get_context_data(self):
+        context = super(SmallSelectWidget, self).get_context_data()
+        context.update({
+            'classes': ['input-sm']
+        })
+        return context
 
 class SnapshotForm(forms.Form):
     name = forms.CharField(label='Snapshot name',
@@ -15,4 +23,11 @@ class SnapshotForm(forms.Form):
     filesystem = forms.TypedChoiceField(
         label='Parent filesystem',
         choices=filesystem_choices,
+    )
+
+
+class FileBrowserForm(forms.Form):
+    filesystem = forms.ChoiceField(
+        choices=filesystem_choices,
+        widget=SmallSelectWidget
     )
