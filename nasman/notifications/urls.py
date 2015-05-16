7824e391f2ee66e9ca74cafeaac90fc19e17ae61
Ben Cole <wengole@gmail.com>
2015-05-16 10:43:40 +0100

    Need to provide base_name

--- nasman/notifications/urls.py
+++ nasman/notifications/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r'messages', views.NotificationViewSet)
+router.register(r'messages', views.NotificationViewSet, base_name='messages')
 
 urlpatterns = router.urls
