cee5350c2003039b36f20c3dac794ceacbfffba9
Ben Cole <wengole@gmail.com>
2015-05-16 10:43:19 +0100

    Add created date to serializer

--- nasman/notifications/serializers.py
+++ nasman/notifications/serializers.py
@@ -6,4 +6,4 @@ from .models import Notification
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
-        fields = ('message', )
+        fields = ('message', 'created', )
