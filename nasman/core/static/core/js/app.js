1c337438a6fbb71a54cdbe24993c9988818646f7
Ben Cole <wengole@gmail.com>
2015-05-17 20:20:19 +0100

    Switched theme to Inspinia Seed (barebones)

---
+++ nasman/core/static/core/js/app.js
@@ -0,0 +1,13 @@
+/**
+ * INSPINIA - Responsive Admin Theme
+ * Copyright 2015 Webapplayers.com
+ *
+ */
+(function () {
+    angular.module('inspinia', [
+        'ui.router',                    // Routing
+        'oc.lazyLoad',                  // ocLazyLoad
+        'ui.bootstrap',                 // Ui Bootstrap
+    ])
+})();
+
--- nasman/core/static/core/js/controllers.js
+++ nasman/core/static/core/js/controllers.js
@@ -1,53 +1,20 @@
-var NasManControllers = angular.module('NasManControllers', []);
+/**
+ * MainCtrl - controller
+ */
+function MainCtrl() {
 
-NasManControllers.controller(
-    'NotificationListCtrl',
-    ['$scope', '$dragon',
-        function ($scope, $dragon) {
-            $scope.notifications = [];
-            $scope.channel = 'notifications';
+    this.userName = 'Example user';
+    this.helloText = 'Welcome in SeedProject';
+    this.descriptionText = 'It is an application skeleton for a typical AngularJS web app. You can use it to quickly bootstrap your angular webapp projects and dev environment for these projects.';
 
-            $dragon.onReady(function () {
-                $dragon.subscribe(
-                    'notifications',
-                    $scope.channel).then(
-                    function (response) {
-                        $scope.dataMapper = new DataMapper(response.data);
-                    });
+}
 
-                $dragon.getList(
-                    'notifications').then(
-                    function (response) {
-                        $scope.notifications = response.data;
-                        $scope.notification_count = response.data.length;
-                        $scope.plural = Boolean(response.data.length != 1);
-                    });
 
-                $dragon.callRouter('get_count', 'notifications').then(
-                    function () {
-                        $scope.notification_count = response.data.length;
-                        $scope.plural = Boolean(response.data.length != 1);
-                    }
-                );
-            });
+function NotificationCtrl() {
+    this.count = 5;
 
-            $dragon.onChannelMessage(
-                function (channels, message) {
-                    if (indexOf.call(channels, $scope.channel) > -1) {
-                        $scope.$apply(function () {
-                            $scope.dataMapper.mapData(
-                                $scope.notifications,
-                                message
-                            );
-                        });
-                    }
-                });
-
-            $scope.itemDone = function (item) {
-                item.done = true != item.done;
-                $dragon.update(
-                    'notifications',
-                    item
-                );
-            }
-        }]);
+}
+angular
+    .module('inspinia')
+    .controller('MainCtrl', MainCtrl)
+    .controller('NotificationCtrl', NotificationCtrl);
